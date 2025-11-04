38041dfd6ace5724d11fc723c3a01176892a3a5a
Maysara <maysaracs1001@gmail.com>
2025-11-04 13:45:14 +0300

    resume stuff

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,7 @@ import User from "../models/User.js";
 import bcrypt from "bcrypt";
 import { response } from "express";
 import jwt from "jsonwebtoken";
+import Resume from "../models/Resume.js";
 
 const generateToken = (userId)=>{
     const token = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: '7d'})
@@ -86,6 +87,21 @@ export const getUserById = async (req , res)=>{
         user.password = undefined;
         return res.status(200).json({user})
 
+    }catch(error){
+        return res.status(400).json({message: error.message})
+    }
+}
+
+// get user resume
+// GET: /api/users/resumes
+
+export const getUserResume = async(req, res) =>{
+    try{
+        const userId = req.userId;
+
+        // return user resumes
+        const resumes = await Resume.find({userId})
+        return res.status(200).json({resumes})
     }catch(error){
         return res.status(400).json({message: error.message})
     }
