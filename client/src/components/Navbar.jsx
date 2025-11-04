e5c38d3da854519aaa52b4c931dca3aebef3a212
Maysara <maysaracs1001@gmail.com>
2025-11-04 16:58:14 +0300

    create resume and login api

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Link, Navigate, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../app/features/authSlice';
 
 const Navbar = () => {
-    const user = {name: 'Maysara Mohamed'}
+
+    const {user} = useSelector(state => state.auth)
+    const dispatch = useDispatch()
+
     const navigate = useNavigate()
     const logoutUser = ()=>{
         navigate('/')
+        dispatch(logout())
     }
   return (
     <div className='shadow bg-white'>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,15 @@
 import { FilePenLineIcon, PencilIcon, PlusIcon, TrashIcon, UploadCloud, UploadCloudIcon, XIcon } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
 import { dummyResumeData } from '../assets/assets'
-import { useNavigate } from 'react-router-dom'
+import { data, useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import api from '../configs/api'
+import toast from 'react-hot-toast'
 
 const Dashboard = () => {
+
+    const {user, token} = useSelector(state => state.auth)
+
     const colors = ["#9333ea", "#d97706", "#dc2626", "#0284c7", "#16a34a"]
     const [allResumes, setAllResumes] = useState([])
     const [showCreateResume, setShowCreateResume] = useState(false)
@@ -18,9 +24,18 @@ const Dashboard = () => {
         setAllResumes(dummyResumeData)
     }
     const createResume = async(event) =>{
-        event.preventDefault()
-        setShowCreateResume(false)
-        navigate(`/app/builder/res123`)
+        try {
+            event.preventDefault()
+            const {data} = await api.post('/api/resumes/create', {title}, {headers: {
+                Authorization: token
+            }})
+            setAllResumes([...allResumes, data.resume])
+            setTitle('')
+            setShowCreateResume(false)
+            navigate(`/app/builder/ ${data.resume._id}`)
+        } catch (error) {
+            toast.error(error?.response?.data?.message || error.message)
+        }
     }
     const uploadResume = async(event) =>{
         event.preventDefault()
